8fc2797b68552bc17150818bfdc8e987e50ebb7c
Ido Zarchi <idozarhi01@gmail.com>
2025-06-04 12:43:19 +0300

    button_component_done

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
       <Button size="sm">Small</Button>
       <Button>Default</Button>
       <Button size="lg">Large</Button>
+      <p className="font-[var(--font-family-mulish)] text-[var(--font-size-lg)] leading-[var(--line-height-lg)] font-[var(--font-weight-bold)]">
+        Large Bold Heading
+      </p>
     </>
   );
 }
