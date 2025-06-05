225c009fa4a1238c2688113ffe9724e7b6553d51
Ido Zarchi <idozarhi01@gmail.com>
2025-06-05 10:51:49 +0300

    Start feature/dropdown branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import "./App.css";
 import { Button } from "./components/ui/Button/button";
 
 function App() {
-  return (
-    <>
-      <Button size="sm">Small</Button>
-      <Button>Default</Button>
-      <Button size="lg">Large</Button>
-      <p className="font-[var(--font-family-mulish)] text-[var(--font-size-lg)] leading-[var(--line-height-lg)] font-[var(--font-weight-bold)]">
-        Large Bold Heading
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
