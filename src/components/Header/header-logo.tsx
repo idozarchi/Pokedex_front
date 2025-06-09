7e08412b4058eba72e2d2b082a5405544c41b59f
Ido Zarchi <idozarhi01@gmail.com>
2025-06-09 10:19:01 +0300

    feat(toolbar): add PageToolbar component and related styles

--- src/components/Header/header-logo.tsx
+++ src/components/Header/header-logo.tsx
@@ -1,16 +1,14 @@
-import * as React from "react";
-
 type HeaderLogoProps = {
   src: string;
   alt: string;
   className?: string;
 };
 
-export const HeaderLogo: React.FC<HeaderLogoProps> = ({
+export const HeaderLogo = ({
   src,
   alt,
   className = "h-40 w-40 object-contain",
-}) => (
+}: HeaderLogoProps) => (
   <div className="flex items-center gap-2">
     <img src={src} alt={alt} className={className} />
   </div>
